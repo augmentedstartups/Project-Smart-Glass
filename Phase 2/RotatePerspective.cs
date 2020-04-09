f0a34f6967603c5a23edff8c11b09cd6a56203b5
Augmented Startups <reigngt09@gmail.com>
2020-04-09 20:12:47 +0200

    Add files via upload

---
+++ Phase 2/RotatePerspective.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePerspective : MonoBehaviour
+{
+    public float calibration = 12.4f;
+    public float calibration_y = 12.4f;
+    public GameObject FaceCube;
+    public Vector3 Zero_Rotation_Position; // Set initial detection as Start rotation
+    public Vector3 Current_Rotation_Position; // Continuous detection position
+    public Vector3 Diff_Rotation_Position; // Continuous detection position
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Zero_Rotation_Position = FaceCube.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Current_Rotation_Position = FaceCube.transform.position;
+        Diff_Rotation_Position = Current_Rotation_Position - Zero_Rotation_Position;
+        this.transform.position = new Vector3((Diff_Rotation_Position.x / 1), (Diff_Rotation_Position.y / 1)+91, -689); 
+      
+        this.transform.eulerAngles = new Vector3((Diff_Rotation_Position.y / calibration_y), -(Diff_Rotation_Position.x / calibration), 0);
+    }
+}
